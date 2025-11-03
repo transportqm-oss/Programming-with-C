827b4e3ff8ebd200ff9ee3aeb5963720261b372e
RYZEN SERIES 4000 <transportqm@gmail.com>
2025-11-04 05:31:34 +0700

    Update 041125

---
+++ Bai15c.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main(){
+	/*
+		Example: function 01
+	*/
+	printf("I'm in main: \n");
+	italy();
+	brazil();
+	argentina();
+	
+	/*
+		Example: function 02
+	*/
+	
+	return 0;
+}
+
+//declare function for example 01
+italy(){
+	printf("I'm in Italy.\n");
+}
+brazil(){
+	printf("I'm in Brazil.\n");
+}
+argentina(){
+	printf("I'm in Argentina.\n");
+}
+
+//declare function for example 02
