6f9c1fdd1df2c6a841f4149771bb633b871e4a69
RYZEN SERIES 4000 <transportqm@gmail.com>
2025-12-19 15:57:35 +0700

    Update Test01.c

--- Test01.c
+++ Test01.c
@@ -17,7 +17,8 @@ void inputList(int arr[], int n){
 void displayList(int arr[], int n, char message[]){
 	int i;
 	
-	printf("%s", message);
+	printf("%s\n", message);
+	
 	for(i = 0; i < n; i++){
 		printf("%d\n", arr[i]);
 	}
@@ -53,16 +54,16 @@ void findMinMax(int arr[], int n){
 		}
 	}
 	
-	printf("\nthe minium value is: %d\n", min);
-	printf("the maxium value is: %d\n", max);
+	printf("\nthe miniumvalue is: %d\n", min);
+	printf("the maxiumvalue is: %d\n", max);
 }
 
 //-----------------------------------------------------//
-//declara function: countVowel
+//declare function: countVowel
 int countVowel(char str[]){
 	int i, count = 0;
 	
-	for(i = 0; str[i] != '\0'; i++){
+	for(i = 0; i < str[i] != '\0'; i++){
 		if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'
 		|| str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U'){
 			count++;
@@ -72,16 +73,64 @@ int countVowel(char str[]){
 	return count;
 }
 
+//-----------------------------------------------------//
+//declare function: inputMatrix
+void inputMatrix(int row, int col, int matrix[row][col]){
+	int i, j;
+	
+	printf("please enter value for the matrix: \n");
+	for(i = 0; i < row; i++){
+		for(j = 0; j < col; j++){
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+}
+
+//declare function: sumMatrix
+void sumMatrix(int row, int col, int A[row][col], int B[row][col], int Sum[row][col]){
+	int *pA = &A[0][0];
+	int *pB = &B[0][0];
+	int *pS = &Sum[0][0];
+	int total = row * col;
+	
+	int i;
+	for(i = 0; i < total; i++){
+		*(pS + i) = *(pA + i) + *(pB + i);
+	}
+}
+
+//declare fuction: displayMatrix
+void displayMatrix(int row, int col, int matrix[row][col]){
+	int i, j;
+	
+	for(i = 0; i < row; i++){
+		for(j = 0; j < col; j++){
+			printf("%d ", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+//-----------------------------------------------------//
+//declare function: call by value
+void swap_value(int a, int b){
+	int temp = a;
+	a = b;
+	b = temp;
+	
+	printf("The list value before call by value: a = %d, b = %d\n", a, b);
+}
+
 int main(){
 	printf("-------------------!!!-------------------\n");
 	/*
 		input value for array, sort list array and find min max
 	*/
-	//declara size array, array
+	//declare size of array and array
 	int n;
 	int arr[100];
 	//input
-	printf("please enter size of array: ");
+	printf("please enter value size of array:");
 	scanf("%d", &n);
 	
 	//call function: inputList
@@ -94,12 +143,12 @@ int main(){
 	displayList(arr, n, "\nthe list number after checking: \n");
 	//call function: findMinMax
 	findMinMax(arr, n);
-	
+
 	printf("-------------------!!!-------------------\n");
 	/*
 		input any word, count number vowel in word
 	*/
-	//declara array
+	//declare array
 	char word[10];
 	//input
 	printf("please enter a string: ");
@@ -109,9 +158,47 @@ int main(){
 	int result = countVowel(word);
 	
 	//display result
-	printf("the word is: %s\nHave counter number of vowel is: %d\n", word, result);
+	printf("the word is: %s\nHave number of vowel in word: %d\n", word, result);
+
+	printf("-------------------!!!-------------------\n");
+	/*
+		input two 02 matrix and show result sum of two matrices
+	*/
+	//declare value for row and column
+	int row, col;
+	//input
+	printf("please enter number for row and column: \n");
+	scanf("%d %d", &row, &col);
+	
+	//declare two matrix
+	int A[row][col], B[row][col], Sum[row][col];
+	
+	//input value for matrix
+	printf("please enter value for Matrix A: \n");
+	inputMatrix(row, col, A);
+	
+	printf("please enter value for Matrix B: \n");
+	inputMatrix(row, col, B);
+	
+	//sum of two matrices
+	sumMatrix(row, col, A, B, Sum);
+	
+	//display
+	printf("\nResult (A + B): \n");
+	displayMatrix(row, col, Sum);
 	
 	printf("-------------------!!!-------------------\n");
+	/*
+		Example call by value
+	*/
+	//declare value
+	int x1 = 10, y1 = 20;
+	swap_value(x1, y1);
+	printf("The list number after call by value: x1 = %d, y1 = %d\n", x1, y1);
+	
+	
+	printf("-------------------!!!-------------------\n");
+	
 	
 	return 0;
 }
